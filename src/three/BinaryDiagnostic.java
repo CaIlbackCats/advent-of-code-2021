9118b123008bfdf42a4cd3a7c08786f1d0107dd8
Pecske <pecske92@gmail.com>
2021-12-05 09:44:23 +0000

    solve part 2

--- src/three/BinaryDiagnostic.java
+++ src/three/BinaryDiagnostic.java
@@ -1,6 +1,7 @@
 package three;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -16,18 +17,17 @@ public class BinaryDiagnostic extends AdventSolver<List<String>> {
       super(INPUT_FOLDER);
     }
 
-    //TODO refactor!
     @Override
     protected int findPartOneResult() {
-      int[] sums = new int [this.processedInput.get(0).length()];
-        this.processedInput.forEach(row -> addValueToSumArray(sums, row));
+      int[] sums = getBitFromCommonCount(this.processedInput, true);
       return getConsumption(sums);
     }
 
     @Override
     protected int findPartTwoResult() {
-      // TODO Auto-generated method stub
-      return 0;
+      int generator = getGeneratorOrScrubberRating(new ArrayList<>(this.processedInput), true);
+      int scrubber = getGeneratorOrScrubberRating(new ArrayList<>(this.processedInput), false);
+      return generator*scrubber;
     }
 
     @Override
@@ -35,22 +35,42 @@ public class BinaryDiagnostic extends AdventSolver<List<String>> {
       return FileHandler.createListFromInput(path);
     }
 
-    private void addValueToSumArray(int [] array,String line){
-      IntStream.range(0, line.length()).forEach(index ->array[index]+= Integer.parseInt(line.charAt(index)+""));
+    //TODO refactor
+    private int getGeneratorOrScrubberRating(List<String> diagnostics, boolean generator){
+      int index =0;
+      while(diagnostics.size()>1){
+          int [] bitValues = getBitFromCommonCount(diagnostics, generator);
+          int currentChar = bitValues[index];
+          for (int i = 0; i < diagnostics.size() && diagnostics.size()>1; i++) {
+              String currentLine = diagnostics.get(i);
+              if(Integer.valueOf(currentLine.charAt(index)+"")!=currentChar){
+                  diagnostics.remove(i);
+                  i= -1;
+              }
+          }
+          index++;
+      }
+    return Integer.valueOf(diagnostics.stream().findFirst().orElseThrow(),2);
     }
 
-    private int convertSumValueToBit(int value,boolean common){
-      int result = (this.processedInput.size()-value<value)?1:0;
-      if(!common){
-        result = (result==1)?0:1;
-      }
-      return result;
+  private int [] getBitFromCommonCount(List<String> diagnostics, boolean common){
+    int [] countArray= new int [this.processedInput.get(0).length()];
+    diagnostics.forEach(line -> addValueToSumArray(countArray, line));
+    if(common){
+       return Arrays.stream(countArray).map(number -> (diagnostics.size()-number<=number)?1:0).toArray();
+    }else{
+      return Arrays.stream(countArray).map(number -> (diagnostics.size()-number<=number)?0:1).toArray();
+    }
+  }
+
+    private void addValueToSumArray(int [] array,String line){
+      IntStream.range(0, line.length()).forEach(index ->array[index]+= Integer.parseInt(line.charAt(index)+""));
     }
 
     private int getConsumption(int [] sums){
       StringBuilder gammaRate = new StringBuilder();
       StringBuilder epsilonRate = new StringBuilder();
-      Arrays.stream(sums).forEach(number -> {gammaRate.append(convertSumValueToBit(number, true)); epsilonRate.append(convertSumValueToBit(number, false));});
+      Arrays.stream(sums).forEach(number -> {gammaRate.append(number); epsilonRate.append((number==1)?0:1);});
       return Integer.valueOf(gammaRate.toString(),2)*Integer.valueOf(epsilonRate.toString(),2);
     }
 
